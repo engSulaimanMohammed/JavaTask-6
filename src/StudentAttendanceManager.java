3ad4b44fe0c1600f43759a8ed2590d40b1221c0e
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:11:47 +0400

    feat: add scanner and attendance map

--- src/StudentAttendanceManager.java
+++ src/StudentAttendanceManager.java
@@ -1,2 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class StudentAttendanceManager {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        HashMap<Integer, Integer> attendance = new HashMap<>();
+    }
 }
