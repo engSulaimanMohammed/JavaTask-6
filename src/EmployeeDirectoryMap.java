e0deca04dff289de7c3bb2438e77a66a14b14c4c
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:03:41 +0400

    feat: add scanner and employee map

--- src/EmployeeDirectoryMap.java
+++ src/EmployeeDirectoryMap.java
@@ -1,2 +1,9 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class EmployeeDirectoryMap {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashMap<Integer, String> employees = new HashMap<>();
+    }
 }
