bd19aab600c2368ec04976cf00e53815ad3ee8b6
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:04:34 +0400

    feat: read and validate student records

--- src/StudentGradesMap.java
+++ src/StudentGradesMap.java
@@ -5,5 +5,12 @@ public class StudentGradesMap {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         HashMap<Integer, Double> studentGrades = new HashMap<>();
+
+        IO.print("Enter number of students: ");
+        int numberOfStudents = input.nextInt();
+
+        if (numberOfStudents <= 0) {
+            IO.println("Invalid number of students.");
+        }
     }
 }
