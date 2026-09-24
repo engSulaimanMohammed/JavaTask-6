6b9c8be4238a813af6c25afae2cf834db7682b0d
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:10:31 +0400

    feat: read and validate employee count

--- src/EmployeeSalaryManager.java
+++ src/EmployeeSalaryManager.java
@@ -5,5 +5,14 @@ public class EmployeeSalaryManager {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         HashMap<Integer, Double> employees = new HashMap<>();
+
+        IO.print("Enter number of employees: ");
+        int numberOfEmployees = input.nextInt();
+
+        if (numberOfEmployees <= 0) {
+            IO.println("Invalid number of employees.");
+            input.close();
+            return;
+        }
     }
 }
