974396884bddaf382e28d86ceac1817c2652d00e
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:05:19 +0400

    feat: add scanner and product price map

--- src/ProductPriceCatalog.java
+++ src/ProductPriceCatalog.java
@@ -1,2 +1,9 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class ProductPriceCatalog {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        HashMap<String, Double> products = new HashMap<>();
+    }
 }
