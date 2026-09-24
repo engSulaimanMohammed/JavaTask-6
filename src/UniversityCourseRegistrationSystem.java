7855dbc015ce6fb3bb80f2da5592e5eefc50ae6a
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:08:49 +0400

    feat: add registration collections

--- src/UniversityCourseRegistrationSystem.java
+++ src/UniversityCourseRegistrationSystem.java
@@ -1,2 +1,11 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class UniversityCourseRegistrationSystem {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        HashSet<Integer> studentIds = new HashSet<>();
+        HashMap<Integer, HashSet<String>> registrations = new HashMap<>();
+    }
 }
