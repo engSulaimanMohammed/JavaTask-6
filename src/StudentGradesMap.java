0dab713a68f5e953ab47633de747500849b95732
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:04:31 +0400

    feat: add scanner and student grades map

--- src/StudentGradesMap.java
+++ src/StudentGradesMap.java
@@ -1,2 +1,9 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class StudentGradesMap {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        HashMap<Integer, Double> studentGrades = new HashMap<>();
+    }
 }
