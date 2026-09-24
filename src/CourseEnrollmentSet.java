b7de5aab4297a6c09b54509d45532a83675b560f
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 09:45:45 +0400

    feat: add scanner and course set

--- src/CourseEnrollmentSet.java
+++ src/CourseEnrollmentSet.java
@@ -1,2 +1,9 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class CourseEnrollmentSet {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashSet<String> Courses = new HashSet<>();
+    }
 }
