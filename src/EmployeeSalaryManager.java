902230d954dcc9f2a9543da2998989ae09f9b41e
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:10:28 +0400

    feat: add scanner and salary map

--- src/EmployeeSalaryManager.java
+++ src/EmployeeSalaryManager.java
@@ -1,2 +1,9 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class EmployeeSalaryManager {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        HashMap<Integer, Double> employees = new HashMap<>();
+    }
 }
