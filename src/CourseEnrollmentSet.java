de8bd0533c867cb1d7b1c47ebdf33192d7a82973
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:01:54 +0400

    feat: read and validate course count

--- src/CourseEnrollmentSet.java
+++ src/CourseEnrollmentSet.java
@@ -5,5 +5,12 @@ public class CourseEnrollmentSet {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         HashSet<String> Courses = new HashSet<>();
+
+        IO.println("Enter The course number: ");
+        int coursenumber = scanner.nextInt();
+
+        if (coursenumber <= 0) {
+            IO.println("Invalid number of courses.");
+        }
     }
 }
