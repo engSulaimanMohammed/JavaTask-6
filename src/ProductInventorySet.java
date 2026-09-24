42451271fcda3f9028543c8366ec278e45ce06af
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:01:08 +0400

    feat: read and validate product count

--- src/ProductInventorySet.java
+++ src/ProductInventorySet.java
@@ -5,5 +5,12 @@ public class ProductInventorySet {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         HashSet<String> Products = new HashSet<>();
+
+        IO.println("Enter The Product Numbers: ");
+        int productnumbers = scanner.nextInt();
+
+        if (productnumbers <= 0) {
+            IO.println("Invalid number of products.");
+        }
     }
 }
