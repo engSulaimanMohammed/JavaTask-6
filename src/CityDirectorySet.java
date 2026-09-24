ad6f2f4668e4cb3e7a0a39dd5f93f494a207bb5e
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:02:40 +0400

    feat: add scanner and city TreeSet

--- src/CityDirectorySet.java
+++ src/CityDirectorySet.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class CityDirectorySet {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TreeSet<String> Cities = new TreeSet<>();
+    }
 }
