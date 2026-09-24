a170e2c19c34dc3f9cced2a34f9df5e5e6975648
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:02:45 +0400

    feat: read and validate city count

--- src/CityDirectorySet.java
+++ src/CityDirectorySet.java
@@ -5,5 +5,13 @@ public class CityDirectorySet {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         TreeSet<String> Cities = new TreeSet<>();
+
+        IO.print("Enter The Number Of Cities: ");
+        int CitiesNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        if (CitiesNumber <= 0) {
+            IO.println("Invalid number of cities.");
+        }
     }
 }
