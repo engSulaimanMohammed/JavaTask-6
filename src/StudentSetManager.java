b2bcac3905f6af98846369e10dce97c4127213b5
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 09:45:15 +0400

    feat: add scanner and student ID set

--- src/StudentSetManager.java
+++ src/StudentSetManager.java
@@ -1,2 +1,9 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class StudentSetManager {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashSet<String> StudentIDs = new HashSet<>();
+    }
 }
