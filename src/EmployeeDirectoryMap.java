99a4f1b033fd26fa22e9c534ceadfe628a3f07be
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:03:55 +0400

    feat: read and validate employee count

--- src/EmployeeDirectoryMap.java
+++ src/EmployeeDirectoryMap.java
@@ -5,5 +5,13 @@ public class EmployeeDirectoryMap {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         HashMap<Integer, String> employees = new HashMap<>();
+
+        IO.print("Enter The Employee Number: ");
+        int EmployeeNum = scanner.nextInt();
+        scanner.nextLine();
+
+        if (EmployeeNum <= 0) {
+            IO.println("Invalid number of employees.");
+        }
     }
 }
