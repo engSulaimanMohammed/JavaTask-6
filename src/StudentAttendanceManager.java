9f93ee14956f3fd06f3d6ac27c5595feec2c7b36
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:11:51 +0400

    feat: read and validate attendance records

--- src/StudentAttendanceManager.java
+++ src/StudentAttendanceManager.java
@@ -6,5 +6,14 @@ public class StudentAttendanceManager {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         HashMap<Integer, Integer> attendance = new HashMap<>();
+
+        IO.print("Enter number of student records: ");
+        int numberOfStudents = input.nextInt();
+
+        if (numberOfStudents <= 0) {
+            IO.println("Invalid number of students.");
+            input.close();
+            return;
+        }
     }
 }
