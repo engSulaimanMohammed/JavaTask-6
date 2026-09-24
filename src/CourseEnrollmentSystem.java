6273bf2180551bc7c15d848a02192bebaba8f003
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:07:42 +0400

    feat: read and validate student records

--- src/CourseEnrollmentSystem.java
+++ src/CourseEnrollmentSystem.java
@@ -7,5 +7,13 @@ public class CourseEnrollmentSystem {
         Scanner scanner = new Scanner(System.in);
         HashSet<Integer> studentIds = new HashSet<>();
         HashMap<Integer, String> studentCourses = new HashMap<>();
+
+        IO.print("Enter number of students: ");
+        int numberOfStudents = scanner.nextInt();
+        scanner.nextLine();
+
+        if (numberOfStudents <= 0) {
+            IO.println("Invalid number of students.");
+        }
     }
 }
