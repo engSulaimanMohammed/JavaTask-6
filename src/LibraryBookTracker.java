72706e1d69a2c30b4adef561b43ceefc69717c05
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:06:07 +0400

    feat: add book ID set and book map

--- src/LibraryBookTracker.java
+++ src/LibraryBookTracker.java
@@ -1,2 +1,11 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class LibraryBookTracker {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        HashSet<Integer> bookIds = new HashSet<>();
+        HashMap<Integer, String> books = new HashMap<>();
+    }
 }
