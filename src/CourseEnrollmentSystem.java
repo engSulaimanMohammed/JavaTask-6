e500fec25d378be7efd19cd1d04b3482319102bc
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:07:35 +0400

    feat: add student set and course map

--- src/CourseEnrollmentSystem.java
+++ src/CourseEnrollmentSystem.java
@@ -1,2 +1,11 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class CourseEnrollmentSystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashSet<Integer> studentIds = new HashSet<>();
+        HashMap<Integer, String> studentCourses = new HashMap<>();
+    }
 }
