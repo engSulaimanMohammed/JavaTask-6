0db482d90a50b4403185b394db8c77b0544ec24b
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 11:05:23 +0400

    feat: read and validate product records

--- src/ProductPriceCatalog.java
+++ src/ProductPriceCatalog.java
@@ -5,5 +5,13 @@ public class ProductPriceCatalog {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         HashMap<String, Double> products = new HashMap<>();
+
+        IO.print("Enter number of products: ");
+        int numberOfProducts = input.nextInt();
+        input.nextLine();
+
+        if (numberOfProducts <= 0) {
+            IO.println("Invalid number of products.");
+        }
     }
 }
