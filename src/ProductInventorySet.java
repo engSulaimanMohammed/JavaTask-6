0634a5b1f90234a9320d6c04b15746de2002dc10
Sulaiman Mohammed <suliman9mohammed@gmail.com>
2026-09-24 09:45:29 +0400

    feat: add scanner and product set

--- src/ProductInventorySet.java
+++ src/ProductInventorySet.java
@@ -1,2 +1,9 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class ProductInventorySet {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashSet<String> Products = new HashSet<>();
+    }
 }
